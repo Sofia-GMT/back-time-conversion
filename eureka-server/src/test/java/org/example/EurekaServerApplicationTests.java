65de68203dd1003e076242900194b1f7a26dd1c0
Sofia Gonzalez <sofia.gonzalez@roche.com>
2025-11-20 20:07:00 +0100

    eureka server

---
+++ eureka-server/src/test/java/org/example/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // verify spring context loads successfully
+    }
+
+}
+
